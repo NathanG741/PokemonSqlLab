0708bec215853febdf3ef0255c0860ff2de1af69
nathan <nathan.guardado@yahoo.com>
2023-12-02 12:07:31 -0500

    Completed part2

--- pokemon_sql/part2.sql
+++ pokemon_sql/part2.sql
@@ -1,7 +1,8 @@
 --Question 1;
 SELECT * FROM types;
 --Question 2;
-SELECT FROM Pokemons WHERE id = 45;
+SELECT name FROM pokemons WHERE id = 45;
 --Question 3;
-
+SELECT * FROM pokemons;
 --Question 4;
+SELECT * FROM pokemons WHERE secondary_type;
